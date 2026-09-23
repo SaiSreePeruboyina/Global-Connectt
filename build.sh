891340139ec67e3f66f8961b42f6e204d4054c1e
Sai Sree Peruboyina <131800986+SaiSreePeruboyina@users.noreply.github.com>
2026-09-24 01:14:06 +0530

    Created build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+
+export PATH="$HOME/flutter/bin:$PATH"
+
+flutter config --enable-web
+
+flutter pub get
+
+flutter build web --release
